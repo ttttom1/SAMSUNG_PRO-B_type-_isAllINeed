302d7c62efd272812da152ccdc52b8f0eff878d7
ttttom1 <chanhotom@gmail.com>
2026-08-22 21:09:55 +0900

    [Passed] 단순한 동전 챙기기 (284ms, 16MB)

--- codetree/loop1/trail4/단순한 동전 챙기기/collect-coins-easy.java
+++ codetree/loop1/trail4/단순한 동전 챙기기/collect-coins-easy.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 
 class Point {
     int num, r, c;
-    Point(int num, int r, int c) {
+    Point (int num, int r, int c) {
         this.num = num;
         this.r = r;
         this.c = c;
@@ -13,68 +13,57 @@ class Point {
 
 public class Main {
     static int N;
-    static Point start, end;
     static ArrayList<Point> coins = new ArrayList<>();
     static ArrayList<Point> selected = new ArrayList<>();
-    static int minDist = Integer.MAX_VALUE;
-
-    // 맨해튼 거리 계산
-    static int getDist(Point p1, Point p2) {
-        return Math.abs(p1.r - p2.r) + Math.abs(p1.c - p2.c);
-    }
-
-    // 동전 3개를 고르는 백트래킹 (조합)
-    static void findCombination(int idx, int cnt) {
-        if (cnt == 3) {
-            // S -> 동전1 -> 동전2 -> 동전3 -> E 거리 합산
-            int totalDist = getDist(start, selected.get(0))
-                          + getDist(selected.get(0), selected.get(1))
-                          + getDist(selected.get(1), selected.get(2))
-                          + getDist(selected.get(2), end);
-
-            minDist = Math.min(minDist, totalDist);
-            return;
-        }
-
-        if (idx == coins.size()) return;
-
-        // 현재 동전 선택
-        selected.add(coins.get(idx));
-        findCombination(idx + 1, cnt + 1);
-        selected.remove(selected.size() - 1);
-
-        // 현재 동전 미선택
-        findCombination(idx + 1, cnt);
-    }
+    static int minMoves = Integer.MAX_VALUE;
+    static Point start, end;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
 
         for (int i = 0; i < N; i++) {
-            String row = sc.next();
-            for (int j = 0; j < N; j++) {
-                char ch = row.charAt(j);
-                if (ch == 'S') {
-                    start = new Point(0, i, j);
-                } else if (ch == 'E') {
-                    end = new Point(0, i, j);
-                } else if (ch >= '1' && ch <= '9') {
-                    coins.add(new Point(ch - '0', i, j));
+            String line = sc.next();
+            for (int j = 0 ; j < N;j++) {
+                char chr = line.charAt(j);
+                if (chr == 'S') {
+                    start = new Point(0,i,j);
+                } else if (chr == 'E') {
+                    end = new Point(0,i,j);
+                } else if (chr >='1' && chr <= '9') {
+                    coins.add(new Point(chr - '0',i,j));
                 }
             }
         }
+        // Please write your code here.
+        Collections.sort(coins,(a,b) -> a.num - b.num);
+        findMinMoves(0, 0);
 
-        // 동전 번호 오름차순 정렬
-        Collections.sort(coins, (a, b) -> a.num - b.num);
-
-        findCombination(0, 0);
-
-        // 3개 이상 수집이 불가능한 경우 -1 출력
-        if (minDist == Integer.MAX_VALUE) {
+        if (minMoves == Integer.MAX_VALUE) {
             System.out.println(-1);
         } else {
-            System.out.println(minDist);
+            System.out.println(minMoves);
         }
+
+    }
+
+    static void findMinMoves(int idx, int cnt) {
+        if (cnt == 3) {
+            int dist = getDist(start,selected.get(0)) + getDist(selected.get(0), selected.get(1)) +
+            getDist(selected.get(1), selected.get(2))  + getDist(selected.get(2),end);
+            minMoves = Math.min(minMoves,dist);
+            return;
+        }
+        if (idx == coins.size()) return;
+
+        selected.add(coins.get(idx));
+        findMinMoves(idx+1, cnt+1);
+        selected.remove(selected.size() - 1);
+
+        findMinMoves(idx+1,cnt);
+    }
+
+    static int getDist(Point p1, Point p2) {
+        return Math.abs(p1.r - p2.r) + Math.abs(p1.c - p2.c);
     }
 }
