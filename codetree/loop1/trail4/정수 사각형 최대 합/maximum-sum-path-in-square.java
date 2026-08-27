77608892cf9f6c460bf4df5b38c8138498d3bff4
ttttom1 <chanhotom@gmail.com>
2026-08-27 13:50:40 +0900

    [Passed] 정수 사각형 최대 합 (133ms, 10MB)

---
+++ codetree/loop1/trail4/정수 사각형 최대 합/maximum-sum-path-in-square.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    static int n, max;
+    static int[][] matrix;
+    static long[][] dp;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        
+        matrix = new int[n][n];
+        dp = new long[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        // Please write your code here.
+        
+        init();
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <n; j++) {
+                dp[i][j] = Math.max(dp[i-1][j] + matrix[i][j] , dp[i][j-1] + matrix[i][j]);
+            }
+        }
+
+        System.out.print(dp[n-1][n-1]);
+
+    }
+
+    static void init() {
+        dp[0][0] = matrix[0][0];
+        for (int i = 1 ; i < n;i++) {
+            dp[i][0] = dp[i-1][0] + matrix[i][0];
+            dp[0][i] = dp[0][i-1] + matrix[0][i]; 
+        }
+    }
+}
