77d00a52e144b688a5ba5d1b5fa17bb85d84ff78
ttttom1 <chanhotom@gmail.com>
2026-08-27 13:20:13 +0900

    [Passed] 서로 다른 BST 개수 세기 (124ms, 10MB)

--- codetree/loop1/trail4/서로 다른 BST 개수 세기/number-of-unique-bst.java
+++ codetree/loop1/trail4/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -1,20 +1,39 @@
 import java.util.Scanner;
 public class Main {
+
+    public static final int UNUSED =-1;
+    public static final int MAX_NUM = 19;
+
+    public static int[] memo = new int[MAX_NUM + 1];
+
+    public static void initializeMemo() {
+        for (int i=0; i <= MAX_NUM;i++) {
+            memo[i] = UNUSED;
+        }
+    }
+
+    static int numOfUniqueBST(int n) {
+        if (memo[n] != UNUSED) {
+            return memo[n];
+        }
+        if(n <= 1) {
+            return 1;
+        }
+
+        int sum = 0;
+        for (int i = 0 ; i < n;i++) {
+            sum += numOfUniqueBST(i) * numOfUniqueBST(n- i -1);
+        }
+
+        return memo[n] = sum;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // Please write your code here.
-        long[] dp = new long[n + 1];
+        initializeMemo();
 
-        dp[0]=1;
-        dp[1] = 1;
-        
-        for (int i = 2; i <=n;i++) {
-            for (int j =0;j < i;j++) {
-                dp[i] += dp[j] * dp[i -  1 -j];
-            }
-        }
-
-        System.out.println(dp[n]);
+        System.out.println(numOfUniqueBST(n));
     }
 }
