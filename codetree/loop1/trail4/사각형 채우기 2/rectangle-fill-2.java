57b90e85ad11e7690972242d100a1f3493bb652a
ttttom1 <chanhotom@gmail.com>
2026-08-27 13:28:26 +0900

    [Passed] 사각형 채우기 2 (138ms, 10MB)

---
+++ codetree/loop1/trail4/사각형 채우기 2/rectangle-fill-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+
+        long[]  dp = new long[1001];
+
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for (int i = 3;i <= n ; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] *2) % 10007;
+        }
+
+        System.out.print(dp[n]);
+
+    }
+}
